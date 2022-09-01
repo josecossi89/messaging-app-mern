dcadebf392da1aafd2af554aac813a937b9a9023
Jose Cossi <josecossib@gmail.com>
2022-09-01 11:52:54 -0300

    reducer added

---
+++ messaging-app-frontend/src/components/reducer.js
@@ -0,0 +1,20 @@
+export const initialState = { use: null };
+
+export const actionsTypes = {
+  SET_USER: "SET_USER",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case actionsTypes.SET_USER:
+      return {
+        ...state,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
