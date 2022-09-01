b11cacfda839a7e2f2cab1281a10c7cccdaa74c5
Jose Cossi <josecossib@gmail.com>
2022-09-01 10:21:51 -0300

    Popup google added for login

--- messaging-app-frontend/src/components/Login.js
+++ messaging-app-frontend/src/components/Login.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Button } from "@material-ui/core";
-
+import { auth, provider } from "../firebase.js";
 import "./Login.css";
 
 const Login = () => {
-  const signIn = () => {};
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => console.log(result))
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="login">
