8dc8c52ced69a72b48dbb937b1aacd06c120e420
Jose Cossi <josecossib@gmail.com>
2022-09-01 10:21:17 -0300

    corrections to imports

--- messaging-app-frontend/src/firebase.js
+++ messaging-app-frontend/src/firebase.js
@@ -1,8 +1,8 @@
-import firebase from "firebase/app";
-import "firebase/auth"; // for authentication
-import "firebase/storage"; // for storage
-import "firebase/database"; // for realtime database
-import "firebase/firestore"; // for cloud firestore
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"; // for authentication
+import "firebase/compat/storage"; // for storage
+import "firebase/compat/database"; // for realtime database
+import "firebase/compat/firestore"; // for cloud firestore
 
 const firebaseConfig = {
   apiKey: "AIzaSyDHccQ8W02e1t-r6GrJxFNs4ikT0GS3brI",
@@ -21,5 +21,5 @@ const auth = firebase.auth();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
-export { auth, provider };
 export default db;
+export { auth, provider };
