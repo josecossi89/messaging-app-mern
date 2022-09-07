f4979468699b67dd25f06b5a074f416ddcef0883
Jose Cossi <josecossib@gmail.com>
2022-09-07 09:24:57 -0300

    props added to test

--- messaging-app-frontend/src/components/SidebarChat.js
+++ messaging-app-frontend/src/components/SidebarChat.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Avatar } from "@mui/material";
 import "./SidebarChat.css";
 
-const SidebarChat = () => {
+const SidebarChat = ({ messages }) => {
   const [seed, setSeed] = useState("");
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
@@ -11,8 +11,8 @@ const SidebarChat = () => {
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
       <div className="sidebarChat__info">
-        <h2>Room name</h2>
-        <p>Last message...</p>
+        <h2>Dev Help</h2>
+        <p>{messages[messages.length - 1]?.messages}</p>
       </div>
     </div>
   );
