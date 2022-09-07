269520d1aa85f33af971bd4d0ada8b9bf3839737
Jose Cossi <josecossib@gmail.com>
2022-09-06 23:39:06 -0300

    corrections_2

--- messaging-app-frontend/src/index.js
+++ messaging-app-frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { StateProvider } from "./StateProvider";
+import { StateProvider } from "./components/StateProvider";
 import reducer, { initialState } from "./components/reducer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
