e89e4cd921383f1abe0453dc79d4d959ae895891
Jose Cossi <josecossib@gmail.com>
2022-09-07 09:24:36 -0300

    prop added in chats

--- messaging-app-frontend/src/components/Chat.js
+++ messaging-app-frontend/src/components/Chat.js
@@ -9,14 +9,17 @@ import {
 import MicIcon from "@mui/icons-material/Mic";
 import "./Chat.css";
 import axios from "./axios";
+import { useStateValue } from "./StateProvider";
+
 const Chat = ({ messages }) => {
   const [seed, setSeed] = useState("");
   const [input, setInput] = useState("");
+  const [{ user }, dispatch] = useStateValue();
   const sendMessage = async (e) => {
     e.preventDefault();
     await axios.post("/messages/new", {
       message: input,
-      name: "josecossib",
+      name: user.displayName,
       timestamp: new Date().toUTCString(),
       received: true,
     });
@@ -30,8 +33,8 @@ const Chat = ({ messages }) => {
       <div className="chat__header">
         <Avatar src={`https://avatars.dicebear.com/api/human/b${seed}.svg`} />
         <div className="chat__headerInfo">
-          <h3>Room Name</h3>
-          <p>Last seen at...</p>
+          <h3>Dev Help</h3>
+          <p>Last seen at {messages[messages.length - 1]?.timestamp}</p>
         </div>
         <div className="chat__headerRight">
           <IconButton>
@@ -48,7 +51,9 @@ const Chat = ({ messages }) => {
       <div className="chat__body">
         {messages.map((message) => (
           <p
-            className={`chat__message ${message.received && "chat__receiver"}`}
+            className={`chat__message ${
+              message.name === user.displayName && "chat__receiver"
+            }`}
           >
             <span className="chat__name">{message.name}</span>
             {message.message}
